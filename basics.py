eb7c733c4ef6339e6205a3989eca64548ddbc5e6
sree <sreena.p01@gmail.com>
2021-11-28 11:20:39 +0530

    python programs

---
+++ basics.py
@@ -0,0 +1,23 @@
+#Program to add two numbers
+a=10
+b=20
+print(a+b)
+
+#Program to know if a number is even or odd.
+n = input( " Enter a number: ")
+x = int(n)
+r = x % 2
+if r == 0:
+    print(" Even ")
+else:
+    print( " Odd ")
+
+# MULTIPLY TWO NUMBERS
+h = input( " Enter number 1 ")
+h = int(h)
+i = input( " Enter number 2 ")
+i = int(i)
+r = h*i
+print( " The answer is -",r )
+
+
---
+++ patterns.py
@@ -0,0 +1,6 @@
+a= input("Enter a number:")
+a=int(a)
+if(a>10):
+    print("Number is greater than 10")
+else:
+    print("Number is less than 10")
